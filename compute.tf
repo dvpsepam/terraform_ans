9c2413c0f2774830bd7af63f30851fe5b4191953
Andrii.Rusin <aws.motolok@gmail.com>
2023-03-08 21:29:56 +0000

    fix

---
+++ compute.tf
@@ -0,0 +1,11 @@
+data "aws_ami" "serv_ami" {
+    # most_recent = true
+
+    # owners = ["099720109477"]
+    
+    # filter {
+    #     name = "name"
+    #     values = ["amazon/ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    # }
+}
+
